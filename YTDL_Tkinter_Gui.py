b965cb3441bcf2335516135fdf6a245cce38db58
Aniket Roy Chowdhury <aniket.roychow@gmail.com>
2021-01-06 20:10:31 -0500

    Initial commit

---
+++ YTDL_Tkinter_Gui.py
@@ -0,0 +1,23 @@
+from __future__ import unicode_literals
+import youtube_dl
+from tkinter import *
+from tkinter import ttk
+
+tkinter._test()
+
+class YTDLGooey:
+    def __init__(self, root):
+
+        #Window Title
+        root.title("YTDL Gooey")
+
+        #Main Window
+        mainframe = ttk.Frame(root, padding="3 3 12 12")
+        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+
+
+
+
+
---
+++ ytdl-import-testing.py
@@ -0,0 +1,55 @@
+from __future__ import unicode_literals
+import youtube_dl
+from dearpygui.core import *
+from dearpygui.simple import *
+
+#method for adding lines to keep ouput clean (probably will remove later)
+def separator(seplength=45):
+    separator = ''
+    for i in range(0, seplength):
+        separator = separator + '-'
+    print(separator)
+
+#logger that only outputs errors, (can also do debug and warnings)
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+    def warning(self, msg):
+        pass
+    def error(self, msg):
+        print(msg)
+
+#prints status of ffmpeg/ytdl
+def my_hook(d):
+    # #TODO - see if 'downloadspam' fix is still necessary to GUI implementation
+        # if d['status'] == 'downloading':
+        #     print('\nDownloading...')
+
+    if d['status'] == 'finished':
+        print('Downloaded to ' + d['filename'] + '\n Converting...\n')
+
+userdir = input("\nEnter directory to save downloads in:\n")
+separator()
+userlink = input("Link to convert: \n")
+separator() 
+
+#main options (secret sauce)
+ytdl_opts = {
+    'format':'bestaudio/best', #best available audio format / best overall
+    'outtmpl': userdir + '\\' + 'vibecheck\\%(title)s.%(ext)s', #output location+template
+    'restrictfilenames': True, #Do not allow "&" and spaces in file names
+   
+    'postprocessors': [{ #names post-processor + keyword arguments for it
+        'key': 'FFmpegExtractAudio', #names postprocessor
+        'preferredcodec': 'mp3', #self explanatory
+        'preferredquality': '320', #320kbps
+        }],
+    
+    'logger' : MyLogger(), #error logging (method made above) 
+    'progress_hooks' : [my_hook], #progress logging (method made above)
+    #'forcetitle': True, #forces printing title (irrelevant outside cmd)
+}
+
+#actually running ytdl with all the setup done
+with youtube_dl.YoutubeDL(ytdl_opts) as ydl:
+    ydl.download([userlink])
