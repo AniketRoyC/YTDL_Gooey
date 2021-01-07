d42f7ef3ad7c3c3b4db25ae38f1586079a55f193
Aniket Roy Chowdhury <aniket.roychow@gmail.com>
2021-01-07 00:14:51 -0500

    Added main window and linkLabel

--- YTDL_Tkinter_Gui.py
+++ YTDL_Tkinter_Gui.py
@@ -10,13 +10,19 @@ class YTDLGooey:
         #Window Title
         root.title("YTDL Gooey")
 
-        #Main Window
-        mainframe = ttk.Frame(root, padding="3 3 12 12")
-        mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        #FRAME1 --> Will contain all the video information
+            #the actual link, playlist yes/no
+        frame1 = ttk.Labelframe(root, text = 'Video Information', padding ="3 3 12 12")
+        frame1.grid(column=0, row=0, sticky=(N, W, E, S))
         root.columnconfigure(0, weight=1)
         root.rowconfigure(0, weight=1)
 
+        LinkLabel = ttk.Label(frame1, text = 'Video Link:').grid(column=1, row=1, sticky = W)
+
+        #Variables
 
 
-
+root = Tk()
+YTDLGooey(root)
+root.mainloop()
 
